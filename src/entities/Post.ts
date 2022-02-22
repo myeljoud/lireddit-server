0fba842c049cec3e6cee4975575e826f2d36e0b0
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2022-02-22 16:01:23 +0000

    Updating resolvers, adding author, points & preview to posts

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,18 +1,43 @@
 import { Field, Int, ObjectType } from "type-graphql";
 import {
-  Entity,
-  PrimaryGeneratedColumn,
+  BaseEntity,
   Column,
   CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
   UpdateDateColumn,
-  BaseEntity,
 } from "typeorm";
+import { User } from "./User";
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
   @Field(() => Int)
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  authorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  author: User;
+
+  @Field()
+  @Column()
+  body!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
+  @Column({ type: "boolean", default: false })
+  preview: boolean;
 
   @Field(() => String)
   @CreateDateColumn()
@@ -21,12 +46,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column()
-  title: string;
-
-  @Field()
-  @Column("text")
-  body: string;
 }
